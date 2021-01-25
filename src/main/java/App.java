ee786a5ceca5ac2c09af5a8b771efb0f022cd595
Gauss-BRO <novel764@gmail.com>
2021-01-25 15:37:54 +0700

    Stream.of

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,9 @@ import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello test");
+        Stream.of(1, 2, 3, 4, 5, 6, 7, 8)
+                .filter(i -> i > 3)
+                .map(i -> i * 3)
+                .forEach(System.out::println);
     }
 }
